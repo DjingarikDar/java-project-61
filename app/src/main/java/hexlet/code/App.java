2a80c5b078da5aa71de5a93c4139edcad8ceccde
Djandjgava Alexey <dar@it-bis.ru>
2023-02-13 16:52:32 +0200

    modify App.java(add choice game)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,19 @@
 package hexlet.code;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1: Cli.greeting();
+                break;
+            case 0:
+                break;
+            default:
+        }
     }
 }
